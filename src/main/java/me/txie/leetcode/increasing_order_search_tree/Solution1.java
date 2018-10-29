b04d5c1eb2290f82f35d02c12d40d0ddd1a73e71
Tian Xie <xietian@outlook.com>
2018-10-29 22:50:26 +0800

    increasing_order_search_tree

---
+++ src/main/java/me/txie/leetcode/increasing_order_search_tree/Solution1.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - increasing_order_search_tree
+ */
+package me.txie.leetcode.increasing_order_search_tree;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public TreeNode increasingBST(TreeNode root) {
+        return null;
+    }
+}
---
+++ src/test/java/me/txie/leetcode/increasing_order_search_tree/Tester.java
@@ -0,0 +1,129 @@
+/**
+ * Leetcode - increasing_order_search_tree
+ */
+package me.txie.leetcode.increasing_order_search_tree;
+
+import java.util.*;
+
+import com.ciaoshen.leetcode.util.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(Parameterized.class)
+public class Tester {
+
+    /**
+     * =========================== static for every test cases ==============================
+     */
+
+    // Solution instance to test
+    private static Solution solution;
+    // use this Object to print the log (call from slf4j facade)
+    private static final Logger LOGGER = LoggerFactory.getLogger(TesterRunner.class);
+
+    /**
+     * Execute once before any of the test methods in this class.
+     */
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+        /* uncomment to switch solutions */
+        solution = new Solution1();
+        // solution = new Solution2();
+    }
+
+    /**
+     * Execute once after all of the test methods are executed in this class.
+     */
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    /**
+     * Initialize test cases
+     */
+    @Parameters
+    public static Collection<Object[]> testcases() {
+        return Arrays.asList(new Object[][]{
+                {makeTree(new Object[]{5, 3, 6, 2, 4, null, 8, 1, null, null, null, 7, 9}),
+                        makeTree(new Object[]{1, null, 2, null, 3, null, 4, null, 5, null, 6, null, 7, null, 8, null, 9})}
+        });
+    }
+
+    private static TreeNode makeTree(Object[] objects) {
+        return makeTree(objects, 0);
+    }
+
+    private static TreeNode makeTree(Object[] objects, int i) {
+        if (i < objects.length) {
+            Object o = objects[i];
+            if (o != null) {
+                TreeNode node = new TreeNode((int) o);
+                node.left = makeTree(objects, 2 * i + 1);
+                node.right = makeTree(objects, 2 * i + 2);
+                return node;
+            }
+        }
+        return null;
+    }
+
+    /**=========================== for each test case ============================== */
+
+    /**
+     * Parameters for each test (initialized in testcases() method)
+     * You can change the type of parameters
+     */
+    private TreeNode root;                       // parameter 1
+    private TreeNode expected;                    // parameter 3 (expected answer)
+
+    /**
+     * This constructor must be provided to run parameterized test.
+     */
+    public Tester(TreeNode root, TreeNode expected) {
+        // initialize test parameters
+        this.root = root;
+        this.expected = expected;
+    }
+
+    /**
+     * Execute before each test method in this class is executed.
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Executed as a test case.
+     */
+    @Test
+    public void test() {
+
+        TreeNode actual = solution.increasingBST(root);
+
+        assertThat(actual, is(equalTo(expected)));
+
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("pass unit test!");
+        }
+    }
+
+    /**
+     * Execute after each test method in this class is executed.
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
