feedcefe1f8a1baca49ae43862d20e5b7be8f541
Tian Xie <xietian@outlook.com>
2018-10-29 22:37:12 +0800

    maximum_depth_of_binary_tree

---
+++ src/main/java/me/txie/leetcode/maximum_depth_of_binary_tree/Solution1.java
@@ -0,0 +1,27 @@
+/**
+ * Leetcode - maximum_depth_of_binary_tree
+ */
+package me.txie.leetcode.maximum_depth_of_binary_tree;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+}
