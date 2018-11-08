82011ca88676c92b5c9abff64dc5476cce099649
Tian Xie <xietian@outlook.com>
2018-11-08 21:41:07 +0800

    increasing_order_search_tree

--- src/main/java/me/txie/leetcode/increasing_order_search_tree/Solution2.java
+++ src/main/java/me/txie/leetcode/increasing_order_search_tree/Solution2.java
@@ -2,16 +2,18 @@
  * Leetcode - increasing_order_search_tree
  */
 package me.txie.leetcode.increasing_order_search_tree;
+
 import java.util.*;
+
 import com.ciaoshen.leetcode.util.*;
 
-/** 
+/**
  * log instance is defined in Solution interface
  * this is how slf4j will work in this class:
  * =============================================
- *     if (log.isDebugEnabled()) {
- *         log.debug("a + b = {}", sum);
- *     }
+ * if (log.isDebugEnabled()) {
+ * log.debug("a + b = {}", sum);
+ * }
  * =============================================
  */
 class Solution2 implements Solution {
@@ -19,6 +21,22 @@ class Solution2 implements Solution {
 
     @Override
     public TreeNode increasingBST(TreeNode root) {
-        return null;
+        List<Integer> vals = new ArrayList<>();
+        inOrder(root, vals);
+        TreeNode newRoot = new TreeNode(0);
+        TreeNode current = newRoot;
+        for (int v : vals) {
+            current.right = new TreeNode(v);
+            current = current.right;
+        }
+        return newRoot.right;
+    }
+
+    private void inOrder(TreeNode node, List<Integer> vals) {
+        if (node == null) return;
+
+        inOrder(node.left, vals);
+        vals.add(node.val);
+        inOrder(node.right, vals);
     }
 }
