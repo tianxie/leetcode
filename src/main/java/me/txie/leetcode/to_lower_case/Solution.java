7796c65ffed8a4f26b8cc2946e49f3868b22f712
Tian Xie <xietian@outlook.com>
2018-11-12 21:58:36 +0800

    to_lower_case

---
+++ src/main/java/me/txie/leetcode/to_lower_case/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - to_lower_case
+ */
+package me.txie.leetcode.to_lower_case;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    String toLowerCase(String str);
+}
---
+++ src/main/java/me/txie/leetcode/to_lower_case/Solution1.java
@@ -0,0 +1,30 @@
+/**
+ * Leetcode - to_lower_case
+ */
+package me.txie.leetcode.to_lower_case;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if ('A' <= chars[i] && chars[i] <= 'Z') {
+                chars[i] += 32;
+            }
+        }
+        return new String(chars);
+    }
+}
