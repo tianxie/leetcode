1a141ef8f93b1ab9d635965a6cc796de353e9dab
Tian Xie <xietian@outlook.com>
2018-11-12 21:40:50 +0800

    unique_email_addresses

---
+++ src/main/java/me/txie/leetcode/unique_email_addresses/Solution1.java
@@ -0,0 +1,36 @@
+/**
+ * Leetcode - unique_email_addresses
+ */
+package me.txie.leetcode.unique_email_addresses;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public int numUniqueEmails(String[] emails) {
+        Set<String> s = new HashSet<>();
+        for (String email : emails) {
+            int atIndex = email.indexOf("@");
+            String localName = email.substring(0, atIndex);
+            String domainName = email.substring(atIndex);
+            int plusIndex = localName.indexOf("+");
+            if (plusIndex > -1) {
+                localName = localName.substring(0, plusIndex);
+            }
+            localName = localName.replace(".", "");
+            s.add(localName + domainName);
+        }
+        return s.size();
+    }
+}
---
+++ src/main/java/me/txie/leetcode/unique_email_addresses/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - unique_email_addresses
+ */
+package me.txie.leetcode.unique_email_addresses;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public int numUniqueEmails(String[] emails) {
+        return 0;
+    }
+}
