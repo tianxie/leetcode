8985fda43d17ec3db85b108cf4a117253b6e7f5a
Tian Xie <xietian@outlook.com>
2018-11-28 21:52:58 +0800

    p476_number_complement

---
+++ src/main/java/me/txie/leetcode/p476_number_complement/Solution1.java
@@ -0,0 +1,30 @@
+/**
+ * Leetcode - p476_number_complement
+ */
+package me.txie.leetcode.p476_number_complement;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public int findComplement(int num) {
+        int i = 0, result = 0;
+        while (num != 0) {
+            result += ((num & 1) ^ 1) << i;
+            num >>= 1;
+            i++;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/me/txie/leetcode/p476_number_complement/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - p476_number_complement
+ */
+package me.txie.leetcode.p476_number_complement;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public int findComplement(int num) {
+        return 0;
+    }
+}
