d3b9d6cd401a8e5069df2bb3be7e968c0154a0c4
Tian Xie <xietian@outlook.com>
2018-12-02 12:19:09 +0800

    p344_reverse_string

---
+++ src/main/java/me/txie/leetcode/p344_reverse_string/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - p344_reverse_string
+ */
+package me.txie.leetcode.p344_reverse_string;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    String reverseString(String s);
+}
---
+++ src/main/java/me/txie/leetcode/p344_reverse_string/Solution1.java
@@ -0,0 +1,30 @@
+/**
+ * Leetcode - p344_reverse_string
+ */
+package me.txie.leetcode.p344_reverse_string;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public String reverseString(String s) {
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            char tmp = chars[i];
+            chars[i] = chars[chars.length - 1 - i];
+            chars[chars.length - 1 - i] = tmp;
+        }
+        return new String(chars);
+    }
+}
