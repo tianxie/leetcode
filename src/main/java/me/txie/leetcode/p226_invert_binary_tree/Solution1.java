2d76f805918cf76293393474d73df9646ab8b970
Tian Xie <xietian@outlook.com>
2018-12-02 10:19:12 +0800

    p226_invert_binary_tree

---
+++ src/main/java/me/txie/leetcode/p226_invert_binary_tree/Solution1.java
@@ -0,0 +1,30 @@
+/**
+ * Leetcode - p226_invert_binary_tree
+ */
+package me.txie.leetcode.p226_invert_binary_tree;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        return root;
+    }
+}
---
+++ src/main/java/me/txie/leetcode/p226_invert_binary_tree/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - p226_invert_binary_tree
+ */
+package me.txie.leetcode.p226_invert_binary_tree;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public TreeNode invertTree(TreeNode root) {
+        return null;
+    }
+}
