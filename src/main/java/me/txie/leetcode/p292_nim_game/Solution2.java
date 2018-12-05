23322335760a130d8733d48459248a777e1317d9
Tian Xie <xietian@outlook.com>
2018-12-05 21:54:37 +0800

    p292_nim_game

---
+++ src/main/java/me/txie/leetcode/p292_nim_game/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - p292_nim_game
+ */
+package me.txie.leetcode.p292_nim_game;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public boolean canWinNim(int n) {
+        return false;
+    }
+}
