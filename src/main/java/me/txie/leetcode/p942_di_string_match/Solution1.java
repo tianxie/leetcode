6a67d2b25806a04208fd24b8a1c1c70b70f89727
Tian Xie <xietian@outlook.com>
2018-12-10 21:46:24 +0800

    p942_di_string_match

---
+++ src/main/java/me/txie/leetcode/p942_di_string_match/Solution1.java
@@ -0,0 +1,38 @@
+/**
+ * Leetcode - p942_di_string_match
+ */
+package me.txie.leetcode.p942_di_string_match;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public int[] diStringMatch(String S) {
+        int N = S.length();
+        int[] a = new int[N + 1];
+        int left = 0, right = N;
+
+        for (int i = 0; i < N; i++) {
+            char c = S.charAt(i);
+            if (c == 'I') {
+                a[i] = left++;
+            } else if (c == 'D') {
+                a[i] = right--;
+            }
+        }
+        a[N] = right;
+
+        return a;
+    }
+}
---
+++ src/main/java/me/txie/leetcode/p942_di_string_match/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - p942_di_string_match
+ */
+package me.txie.leetcode.p942_di_string_match;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public int[] diStringMatch(String S) {
+        return new int[0];
+    }
+}
