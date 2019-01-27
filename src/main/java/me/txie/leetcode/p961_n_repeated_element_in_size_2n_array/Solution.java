7b3e952dc20b9278e4946b37b426815bea5c53a9
Tian Xie <xietian@outlook.com>
2019-01-27 18:37:29 +0800

    p961_n_repeated_element_in_size_2n_array

---
+++ src/main/java/me/txie/leetcode/p961_n_repeated_element_in_size_2n_array/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - p961_n_repeated_element_in_size_2n_array
+ */
+package me.txie.leetcode.p961_n_repeated_element_in_size_2n_array;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    int repeatedNTimes(int[] A);
+}
