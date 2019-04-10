239ecefde20376bbb4e30b984638a0a9245cbe40
Tian Xie <xietian@ehousechina.com>
2019-04-10 18:06:02 +0800

    p125_valid_palindrome

---
+++ src/main/java/me/txie/leetcode/p125_valid_palindrome/Solution1.java
@@ -0,0 +1,35 @@
+/**
+ * Leetcode - p125_valid_palindrome
+ */
+package me.txie.leetcode.p125_valid_palindrome;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    // O(n) runtime, O(1) space
+    @Override
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
