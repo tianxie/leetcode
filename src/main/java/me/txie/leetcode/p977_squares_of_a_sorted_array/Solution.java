0f6ecf56915188eade4def913aea2657b6922370
Tian Xie <xietian@outlook.com>
2019-04-14 21:08:59 +0800

    p977_squares_of_a_sorted_array

---
+++ src/main/java/me/txie/leetcode/p977_squares_of_a_sorted_array/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Leetcode - p977_squares_of_a_sorted_array
+ */
+package me.txie.leetcode.p977_squares_of_a_sorted_array;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+interface Solution {
+
+    // use this Object to print the log (call from slf4j facade)
+    static Logger log = LoggerFactory.getLogger(Solution.class);
+    int[] sortedSquares(int[] A);
+}
---
+++ src/main/java/me/txie/leetcode/p977_squares_of_a_sorted_array/Solution1.java
@@ -0,0 +1,36 @@
+/**
+ * Leetcode - p977_squares_of_a_sorted_array
+ */
+package me.txie.leetcode.p977_squares_of_a_sorted_array;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+    // O(n) runtime, O(n) space
+    @Override
+    public int[] sortedSquares(int[] A) {
+        int[] nums = new int[A.length];
+        int curr = A.length - 1;
+        int i = 0, j = A.length - 1;
+        while (i <= j) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                nums[curr--] = A[j] * A[j];
+                j--;
+            } else {
+                nums[curr--] = A[i] * A[i];
+                i++;
+            }
+        }
+        return nums;
+    }
+}
