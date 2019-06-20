853a93d1539968a6ccc55d7584cf0f9359ab3154
Tian Xie <xietian@outlook.com>
2019-06-20 22:55:45 +0800

    Test with Spock

---
+++ src/test/groovy/me/txie/leetcode/p1_two_sum/Solution1Spec.groovy
@@ -0,0 +1,17 @@
+package me.txie.leetcode.p1_two_sum
+
+import spock.lang.Specification
+
+class Solution1Spec extends Specification {
+    def "TwoSum"() {
+        given: "Solution1"
+        Solution solution = new Solution1();
+
+        expect: "Pass unit test"
+        solution.twoSum(nums, target) == expected
+
+        where: "Samples"
+        nums                    | target || expected
+        [2, 7, 11, 15] as int[] | 9      || [0, 1] as int[]
+    }
+}
