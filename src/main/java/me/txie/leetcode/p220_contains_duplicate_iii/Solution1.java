325ff71d04101ca1a27ba56845b7300ca95c1676
Tian Xie <xietian@outlook.com>
2019-06-26 21:31:41 +0800

    p220_contains_duplicate_iii

---
+++ src/main/java/me/txie/leetcode/p220_contains_duplicate_iii/Solution1.java
@@ -0,0 +1,33 @@
+/**
+ * Leetcode - p220_contains_duplicate_iii
+ */
+package me.txie.leetcode.p220_contains_duplicate_iii;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution1 implements Solution {
+
+
+    @Override
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        // TODO
+        return false;
+    }
+
+    private class Node {
+        private int key;
+        private int val;
+        private Node left, right;
+
+
+    }
+}
---
+++ src/main/java/me/txie/leetcode/p220_contains_duplicate_iii/Solution2.java
@@ -0,0 +1,24 @@
+/**
+ * Leetcode - p220_contains_duplicate_iii
+ */
+package me.txie.leetcode.p220_contains_duplicate_iii;
+import java.util.*;
+import com.ciaoshen.leetcode.util.*;
+
+/** 
+ * log instance is defined in Solution interface
+ * this is how slf4j will work in this class:
+ * =============================================
+ *     if (log.isDebugEnabled()) {
+ *         log.debug("a + b = {}", sum);
+ *     }
+ * =============================================
+ */
+class Solution2 implements Solution {
+
+
+    @Override
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        return false;
+    }
+}
