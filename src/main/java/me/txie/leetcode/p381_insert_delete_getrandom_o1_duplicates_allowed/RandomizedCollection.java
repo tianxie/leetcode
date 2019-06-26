99d570cf1e0b8dc71d64863a832cbc4420c45ca7
Tian Xie <xietian@outlook.com>
2019-06-26 21:33:36 +0800

    p381_insert_delete_getrandom_o1_duplicates_allowed

---
+++ src/main/java/me/txie/leetcode/p381_insert_delete_getrandom_o1_duplicates_allowed/RandomizedCollection.java
@@ -0,0 +1,37 @@
+package me.txie.leetcode.p381_insert_delete_getrandom_o1_duplicates_allowed;
+
+import java.util.Random;
+
+class RandomizedCollection {
+
+    private final Random random;
+
+    /** Initialize your data structure here. */
+    public RandomizedCollection() {
+        random = new Random();
+        // TODO
+    }
+
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    public boolean insert(int val) {
+        return false;
+    }
+
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    public boolean remove(int val) {
+        return false;
+    }
+
+    /** Get a random element from the collection. */
+    public int getRandom() {
+        return 0;
+    }
+}
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * RandomizedCollection obj = new RandomizedCollection();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
