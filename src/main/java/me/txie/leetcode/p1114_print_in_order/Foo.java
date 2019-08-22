7bd252602dd26b190098190ad349483cd20b6fdc
Tian Xie <xietian@outlook.com>
2019-08-22 22:32:48 +0800

    p1114_print_in_order

---
+++ src/main/java/me/txie/leetcode/p1114_print_in_order/Foo.java
@@ -0,0 +1,33 @@
+package me.txie.leetcode.p1114_print_in_order;
+
+import java.util.concurrent.CountDownLatch;
+
+class Foo {
+    private CountDownLatch second;
+    private CountDownLatch third;
+    
+    public Foo() {
+        second = new CountDownLatch(1);
+        third = new CountDownLatch(1);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        second.countDown();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        second.await();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        third.countDown();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        third.await();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+    }
+}
