5e1f901ba34a06e425d6596e7c4f1026222cf612
tianxie <txie@live.cn>
2021-11-21 14:57:24 +0800

    jzoffer_002

--- src/main/java/me/txie/leetcode/jzoffer_002/Solution1.java
+++ src/main/java/me/txie/leetcode/jzoffer_002/Solution1.java
@@ -2,20 +2,45 @@
  * LeetCode - jzoffer_002
  * https://leetcode-cn.com/problems/add-binary/
  */
- 
+
 package me.txie.leetcode.jzoffer_002;
 
 import java.util.*;
+
 import me.txie.leetcode.util.*;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class Solution1 implements Solution {
-    
+
     @Override
     public String addBinary(String a, String b) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0) {
+            int digitA = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int digitB = (j >= 0) ? b.charAt(j) - '0' : 0;
+            int sum = digitA + digitB + carry;
+            if (sum >= 2) {
+                sum -= 2;
+                carry = 1;
+            } else {
+                // 如果当前位没有进位，记得设置为0，不然carry的值仍是上一位的进位
+                carry = 0;
+            }
+            result.insert(0, sum);
+            i--;
+            j--;
+        }
+
+        // 注意最高位的进位情况
+        if (carry == 1) {
+            result.insert(0, 1);
+        }
+        return result.toString();
     }
 }
-// 时间复杂度：
-// 空间复杂度：
+// 时间复杂度：O(n)
+// 空间复杂度：O(n)
