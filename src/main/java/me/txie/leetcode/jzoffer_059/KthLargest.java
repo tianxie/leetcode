ec8eff3b36712168b5de906e647316c2e4d74d3c
tianxie <txie@live.cn>
2022-05-16 21:02:17 +0800

    jzoffer_059

---
+++ src/main/java/me/txie/leetcode/jzoffer_059/KthLargest.java
@@ -0,0 +1,34 @@
+package me.txie.leetcode.jzoffer_059;
+
+import java.util.PriorityQueue;
+
+public class KthLargest {
+    private final PriorityQueue<Integer> minHeap;
+    private final int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.minHeap = new PriorityQueue<>();
+        this.k = k;
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (minHeap.size() < k) {
+            minHeap.offer(val);
+        } else if (val > minHeap.peek()) {
+            minHeap.poll();
+            minHeap.offer(val);
+        }
+
+        return minHeap.peek();
+    }
+}
+// 时间复杂度：O(logk)
+// 空间复杂度：O(k)
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
